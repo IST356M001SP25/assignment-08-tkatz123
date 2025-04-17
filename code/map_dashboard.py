0d74dcdeb0745e70e56a2cd42a1700b4c1ab4187
Tyler Katz <tmkatz@syr.edu>
2025-04-17 16:23:14 -0400

    Completed the map_dashboard file

--- code/map_dashboard.py
+++ code/map_dashboard.py
@@ -12,3 +12,30 @@ CUSE = (43.0481, -76.1474)  # center of map
 ZOOM = 14                   # zoom level
 VMIN = 1000                 # min value for color scale
 VMAX = 5000                 # max value for color scale
+
+#Reading in DF
+tickets = pd.read_csv('cache/top_locations_mappable.csv')
+
+#Setting the title for the dashboard
+st.title('Locations with parking ticket totals over $1,000')
+
+#Converting the df to a geopandas dataframe
+geo_df = gpd.GeoDataFrame(tickets, geometry= gpd.points_from_xy(tickets.lon, tickets.lat))
+
+#Specificy the starting center coordinates for the map, and the zoom level
+map = folium.Map(location = CUSE, zoom_start= ZOOM)
+
+
+ticekts_map = geo_df.explore( 
+    geo_df['amount'],           #Color the markers based on amount values
+    m = map,                    #Adds to the map created above
+    legend = True,              #Specifies to display the legend with color scale of markers
+    legend_name = 'Amount',     #Names the legend amound
+    marker_type= 'circle',      #Specifies to use circle markers
+    marker_kwds= {'radius': 15, #Makes each marker have radius of 15 pixes
+                  'fill': True} #Fills in the cirlce
+
+)
+
+#Embeds map into the streamlit widget for displaying
+sf.folium_static(ticekts_map, width = 1000, height = 800)
